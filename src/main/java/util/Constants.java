861a9b921335a7e174e80223b3c98ba358406730
Marcofdoom <marcvpartington@gmail.com>
2019-06-07 10:23:08 +0100

    first commit with structure of project

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,8 @@
+package util;
+
+public class Constants {
+
+	// EXAMPLE
+	// public static final String ACCOUNT_ONE =
+	// "{\"id\":1,\"accountNumber\":1,\"firstName\":\"Steve\",\"lastName\":\"Steverson\"}";
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Account (id, accountNumber, firstName, lastName) VALUES (1234, 1234, 'Marc', 'Partington');
+INSERT INTO Account (id, accountNumber, firstName, lastName) VALUES (1235, 1235, 'Steve', 'McSteverson');
+INSERT INTO Account (id, accountNumber, firstName, lastName) VALUES (1236, 1236, 'Stone Cold', 'Steve Austin')
