849e2f5fa5f9e901dea52370cb4f4043c78a74e1
Marcofdoom <marcvpartington@gmail.com>
2019-06-07 11:08:18 +0100

    TrainerServce interface created

--- src/main/java/business/service/TrainerService.java
+++ src/main/java/business/service/TrainerService.java
@@ -2,4 +2,11 @@ package business.service;
 
 public interface TrainerService {
 
+	String addTrainer(String name);
+
+	String removeTrainer(String name);
+
+	String updateTrainerDetails(int classroomId);
+
+	String returnTrainerDetails(int classroomId);
 }
