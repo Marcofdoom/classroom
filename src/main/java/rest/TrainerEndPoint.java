6389a96951c61f2f993ee275b22a4026ba88a86d
Marcofdoom <marcvpartington@gmail.com>
2019-06-07 11:21:24 +0100

    TrainerEndPoint class added

--- src/main/java/rest/TrainerEndPoint.java
+++ src/main/java/rest/TrainerEndPoint.java
@@ -1,5 +1,23 @@
 package rest;
 
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import business.service.TrainerService;
+
+@Path("/trainer")
 public class TrainerEndPoint {
 
+	@Inject
+	private TrainerService trainerService;
+
+	@Path("/getTrainer/{id}")
+	@GET
+	@Produces({ "application/json" })
+	public String getTrainer(@PathParam("id") int classroomId) {
+		return null;
+	}
 }
