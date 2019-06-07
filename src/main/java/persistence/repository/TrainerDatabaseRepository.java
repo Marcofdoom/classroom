4cafe402a5d1cea6282927687de24a782311c317
Marcofdoom <marcvpartington@gmail.com>
2019-06-07 10:54:29 +0100

    TrainerDatabaseRepository created

--- src/main/java/persistence/repository/TrainerDatabaseRepository.java
+++ src/main/java/persistence/repository/TrainerDatabaseRepository.java
@@ -1,5 +1,45 @@
 package persistence.repository;
 
-public class TrainerDatabaseRepository {
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
 
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
+public class TrainerDatabaseRepository implements TrainerRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager entityManager;
+
+	@Inject
+	private JSONUtil jsonUtil;
+
+	@Transactional(REQUIRED)
+	public String addTrainer(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Transactional(REQUIRED)
+	public String removeTrainer(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateTrainerDetails(int classroomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String returnTrainerDetails(int classroomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
