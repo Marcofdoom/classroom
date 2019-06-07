ee3cb040666a87d33610b62cb059d099990b6daf
Marcofdoom <marcvpartington@gmail.com>
2019-06-07 10:28:24 +0100

    Trainer class added

--- src/main/java/persistence/domain/Trainer.java
+++ src/main/java/persistence/domain/Trainer.java
@@ -1,5 +1,19 @@
 package persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Trainer {
 
+	@Id
+	private int classroomID;
+
+	private String trainer;
+
+	// TODO create an array of Trainees
+
+	public Trainer() {
+
+	}
 }
